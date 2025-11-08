483166c5ac9be2f961a068d0b601706f97cb4f5a
xSimix <chamorromatoscarlos@gmail.com>
2025-11-07 20:47:19 -0500

    Revert "Add dedicated web pages for destinations, circuits, and packages"

--- web/circuito.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/configuracion/arranque.php';
-
-use Aplicacion\Controladores\ControladorCircuitos;
-
-$controlador = new ControladorCircuitos();
-$controlador->index();
