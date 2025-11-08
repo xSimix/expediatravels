d51d0055c38ec96ccf065e60dde646d616ec398c
xSimix <chamorromatoscarlos@gmail.com>
2025-11-08 14:45:59 -0500

    Redesign site footer to match new layout

--- app/vistas/inicio.php
+++ app/vistas/inicio.php
@@ -769,42 +769,7 @@
         </section>
     </main>
 
-    <footer class="site-footer">
-        <div class="site-footer__brand"><?= htmlspecialchars($siteTitle); ?></div>
-        <div class="site-footer__links">
-            <div>
-                <h4>Explora</h4>
-                <ul>
-                    <li><a href="#paquetes">Paquetes</a></li>
-                    <li><a href="#destinos">Destinos</a></li>
-                    <li><a href="explorar.php">Experiencias</a></li>
-                </ul>
-            </div>
-            <div>
-                <h4>Nosotros</h4>
-                <ul>
-                    <li><a href="#">Quiénes somos</a></li>
-                    <li><a href="#">Trabaja con nosotros</a></li>
-                    <li><a href="#">Prensa</a></li>
-                </ul>
-            </div>
-            <div>
-                <h4>Ayuda</h4>
-                <ul>
-                    <li><a href="#">Centro de soporte</a></li>
-                    <li><a href="#">Políticas de viaje</a></li>
-                    <li>
-                        <?php if (!empty($primaryEmail)): ?>
-                            <a href="mailto:<?= htmlspecialchars($primaryEmail, ENT_QUOTES); ?>">Contacto</a>
-                        <?php else: ?>
-                            <a href="#contacto">Contacto</a>
-                        <?php endif; ?>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <p class="site-footer__legal">© <?= date('Y'); ?> <?= htmlspecialchars($siteTitle); ?>. Todos los derechos reservados.</p>
-    </footer>
+    <?php include __DIR__ . '/partials/site-footer.php'; ?>
 
     <div class="auth-modal" data-auth-modal hidden>
         <div class="auth-modal__backdrop" data-auth-close></div>
